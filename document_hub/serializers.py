d655f50ee8ce28893fb63c6a311b82ef968a5953
nairtanmay <nairtanmaybiju@email.com>
2026-02-24 14:35:42 +0530

    8

--- document_hub/serializers.py
+++ document_hub/serializers.py
@@ -4,11 +4,11 @@ from .models import Document # (Or whatever your model is named)
 class DocumentSerializer(serializers.ModelSerializer):
     # 1. Add this custom field
     file_size = serializers.SerializerMethodField()
-
+    uploaded_by_username = serializers.CharField(source='uploaded_by.username', read_only=True)
     class Meta:
         model = Document
         # 2. Make sure 'file_size' is included in your fields list
-        fields = ['id', 'title', 'description', 'file', 'file_size', 'uploaded_by', 'uploaded_at'] 
+        fields = ['id', 'title', 'description', 'file', 'file_size', 'uploaded_by', 'uploaded_at', 'uploaded_by_username'] 
 
     # 3. Add this method to calculate the size in bytes
     def get_file_size(self, obj):
